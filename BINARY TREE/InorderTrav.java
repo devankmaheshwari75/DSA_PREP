f8b87e56d8a7220e5b65c7b4a81f0f8e10b61fc4
devankmaheshwari75 <maheshwaridevank@gmail.com>
2025-06-29 12:57:27 +0530

    added new folder

---
+++ BINARY TREE/InorderTrav.java
@@ -0,0 +1,27 @@
+
+public class InorderTrav {
+
+    public static void inorderTrav(TreeNode root) {
+
+        if (root == null) {
+            return;
+
+        }
+        inorderTrav(root.left);
+        System.out.println(root.data);
+        inorderTrav(root.right);
+
+    }
+
+    public static void main(String[] args) {
+
+        CreateBT cb = new CreateBT();
+        TreeNode root = cb.createBT_BFS();
+
+        System.out.println("Inorder traversal of the tree is ");
+
+        inorderTrav(root);
+
+    }
+
+}
